ca5e6077d81956a6684d196ae4a955cac3d5386b
jaimerehbein <rebbeim7@gmail.com>
2018-05-20 05:49:33 -0400

    añade components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
+import { MoreComponent } from './components/more/more.component';
+import { GridcardsComponent } from './components/gridcards/gridcards.component';
+import { ParalaxComponent } from './components/paralax/paralax.component';
+import { FormComponent } from './components/form/form.component';
+import { SeparatorComponent } from './components/separator/separator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarrouselComponent,
+    MoreComponent,
+    GridcardsComponent,
+    ParalaxComponent,
+    FormComponent,
+    SeparatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/gridcards/gridcards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gridcards',
+  templateUrl: './gridcards.component.html',
+  styleUrls: ['./gridcards.component.css']
+})
+export class GridcardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
